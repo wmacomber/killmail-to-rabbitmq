96b122736ba50314df5674f1780ab223e1c52189
Walter Macomber <9421441+wmacomber@users.noreply.github.com>
2023-07-17 19:11:35 -0400

    streamline and separate
    Removed all lookup stuff from publisher, let that happen upstream.  Added mongoose schemas for writing killmail to MongoDB, built separate listener to push that data.

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 const amqplib = require("amqplib");
-const fetch = require("node-fetch");
-const NodeCache = require("node-cache");
 const WS = require("ws");
 
-const VERBOSE = false;
+const VERBOSE = true;
 
 const config = require("./config.json");
-const cacheChar = new NodeCache({
-    stdTTL: 600
-});
-
-async function getCharacter(id) {
-    if (parseInt(id) !== id) throw new Error("ID isn't an integer");
-    const cache = cacheChar.get(id);
-    if (typeof cache !== "undefined") return cache;
-    const url = `https://esi.evetech.net/latest/characters/${id}/?datasource=tranquility`;
-    const res = await fetch(url);
-    const r = await res.json();
-    cacheChar.set(id, r);
-    return r;
-}
 
 /**
  * To do simple HTTP fetch polling:
@@ -32,15 +16,18 @@ async function getCharacter(id) {
  * * send {"action":"sub","channel":"killstream"}
  */
 
+const ts = () => { return (new Date()).toISOString(); }
+const l = (s) => { console.log(`[${ts()}] ${s}`); }
+
 async function main() {
     const uri = `amqp://${config.rabbit.user}:${config.rabbit.pass}@${config.rabbit.host}:${config.rabbit.port}/${config.rabbit.vhost}`;
-    if (VERBOSE) console.log(`RabbitMQ connection::: ${uri}`);
+    if (VERBOSE) l(`RabbitMQ connection::: ${uri}`);
     const rabbit = await amqplib.connect(uri);
     const killfeed = await rabbit.createChannel();
     const ws = new WS("wss://zkillboard.com/websocket/");
 
     ws.on("error", (err) => {
-        console.log("WS ERROR:");
+        l("WS ERROR:");
         console.dir(err, { depth: null });
     });
 
@@ -49,19 +36,33 @@ async function main() {
             action: "sub",
             channel: "killstream"
         }));
+        ws.send(JSON.stringify({
+            action: "sub",
+            channel: "public"
+        }));
     });
 
     ws.on("message", async (data) => {
-        let jsonData = JSON.parse( data.toString("utf-8") );
-        if (VERBOSE) console.log(`KILL: ${jsonData.killmail_time} for ${jsonData.zkb.totalValue.toLocaleString("en-US")} ISK`);
-        jsonData["victim"]["character_data"] = await getCharacter(jsonData["victim"]["character_id"]);
-        for (const k of jsonData["attackers"].keys()) {
-            if (typeof jsonData["attackers"][k]["character_id"] !== "undefined") {
-                const c = await getCharacter(jsonData["attackers"][k]["character_id"]);
-                jsonData["attackers"][k]["character_data"] = c;
-            }
+        let jsonData;
+        try {
+            jsonData = JSON.parse( data.toString("utf-8") );
+        } catch (exc) {
+            l("JSON parse exception");
+            console.dir(data, { depth: null });
+            return;
         }
-        killfeed.sendToQueue(config.rabbit.queue, Buffer.from(JSON.stringify(jsonData))); // wants a buffer
+        if (jsonData["action"] === "tqStatus") {
+            /* jsonData = {
+                action: 'tqStatus',
+                tqStatus: 'ONLINE',
+                tqCount: '18,192',
+                kills: '596'
+            } */
+        };
+        if (typeof jsonData["action"] === "undefined") { //
+            if (VERBOSE) l(`KILL [${jsonData.killmail_id}]: ${jsonData.killmail_time} for ${jsonData.zkb.totalValue.toLocaleString("en-US")} ISK`);
+            killfeed.sendToQueue(config.rabbit.queue, Buffer.from(JSON.stringify(jsonData))); // wants a buffer
+        }
     });
 }
 
---
+++ killmail.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+const AttackerSchema = new mongoose.Schema({
+    "alliance_id": { type: Number },
+    "character_id": { type: Number },
+    "corporation_id": { type: Number },
+    "damage_done": { type: Number },
+    "final_blow": { type: Boolean },
+    "security_status": { type: Number },
+    "ship_type_id": { type: Number },
+    "weapon_type_id": { type: Number }
+});
+
+const ItemSchema = new mongoose.Schema({
+    "flag": { type: Number },
+    "item_type_id": { type: Number },
+    "quantity_dropped": { type: Number },
+    "singleton": { type: Number }
+});
+
+const VictimSchema = new mongoose.Schema({
+    "alliance_id": { type: Number },
+    "character_id": { type: Number },
+    "corporation_id": { type: Number },
+    "damage_taken": { type: Number },
+    "items": { type: [ ItemSchema ] },
+    "position": {
+        "x": { type: Number },
+        "y": { type: Number },
+        "z": { type: Number }
+    },
+    "ship_type_id": { type: Number }
+});
+
+const KillmailSchema = new mongoose.Schema({
+    "attackers": { type: [ AttackerSchema ] },
+    "killmail_id": {
+        type: Number,
+        index: {
+            unique: true,
+            dropDups: true
+        }
+    },
+    "killmail_time": {
+        type: Date,
+        index: { expires: "1m" }
+    },
+    "solar_system_id": { type: Number },
+    "victim": { type: VictimSchema },
+    "zkb": {
+        "locationID": { type: Number },
+        "hash": { type: String },
+        "fittedValue": { type: Number },
+        "droppedValue": { type: Number },
+        "destroyedValue": { type: Number },
+        "totalValue": { type: Number },
+        "points": { type: Number },
+        "npc": { type: Boolean },
+        "solo": { type: Boolean },
+        "awox": { type: Boolean },
+        "esi": { type: String },
+        "url": { type: String }
+    }
+});
+
+const KillmailModel = new mongoose.model("killmail", KillmailSchema);
+
+module.exports = {
+    schema: KillmailSchema,
+    model: KillmailModel
+};
